453ef82cc866e4567ef614f02a6a00bfe8d6d4b5
Yura Avramenko <jaerbi42@gmail.com>
2019-07-12 12:45:32 +0300

    Added some UI for navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  links: object = [
+    { title: 'Home', link: '/home' },
+    { title: 'Tasks', link: '/tasks' },
+    { title: 'Login', link: '/login' },
+  ]
 }
